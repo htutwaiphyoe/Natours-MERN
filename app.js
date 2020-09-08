e9e1e772825485fbed5a4eb7df88996b54cc8e95
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-08 18:14:03 +0630

    Adding Global Handler

--- app.js
+++ app.js
@@ -14,10 +14,20 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v2/users", UserRouter);
 app.use("/api/v2/tours", TourRouter);
-app.all("*", (req, res) => {
-    res.status(404).json({
-        result: "fail",
-        message: `Can't find ${req.originalUrl} on this server`,
+app.all("*", (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} in this page`);
+    err.statusCode = 404;
+    err.status = "fail";
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
     });
 });
 module.exports = app;
