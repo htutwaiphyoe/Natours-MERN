7ff987cb922d548bacd3538f78213d3826a96b98
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-09-09 21:51:30 +0630

    Adding Custom Error Class

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const TourRouter = require("./routes/TourRouter");
 const UserRouter = require("./routes/UserRouter");
+const AppError = require("./utils/AppError");
+const ErrorController = require("./controllers/ErrorController");
 
 const app = express();
 if (process.env.NODE_ENV === "development") {
@@ -15,19 +17,8 @@ app.use(express.static(`${__dirname}/public`));
 app.use("/api/v2/users", UserRouter);
 app.use("/api/v2/tours", TourRouter);
 app.all("*", (req, res, next) => {
-    const err = new Error(`Can't find ${req.originalUrl} in this page`);
-    err.statusCode = 404;
-    err.status = "fail";
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} in this page`, 404));
 });
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
-});
+app.use(ErrorController);
 module.exports = app;
