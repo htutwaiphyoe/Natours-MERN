af5dfaae05b06c549e170dba263d3fa1acf61971
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-23 22:05:01 +0630

    Global error middleware

--- app.js
+++ app.js
@@ -24,9 +24,19 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-    res.status(404).json({
-        status: "fail",
-        message: `Can't find ${req.originalUrl} on this server`,
+    const error = new Error(`Can't find ${req.originalUrl} on this server`);
+    error.statusCode = 404;
+    error.status = "fail";
+    next(error);
+});
+
+// global error handing middleware
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
     });
 });
 module.exports = app;
